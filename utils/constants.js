49bb0696f8ca196de1e7244931db6dbe84b54a50
Yuanda Xu <yuandaxu@yahoo.com>
2025-12-09 14:47:12 -0500

    d

--- utils/constants.js
+++ utils/constants.js
@@ -1,8 +1,8 @@
 const GOOGLE_AUTH_SCOPE = ['https://www.googleapis.com/auth/spreadsheets'];
 const GOOGLE_AUTH_KEY_FILE_PATH = `./googleAuth.json`;
 const GOOGLE_SHEET_PROCESS_COST_TABLE_RANGE = "Process Costs!A3:J14";
-const GOOGLE_SHEET_TECH_PERFORMANCE_TABLE_RANGE = "Team Estimates!A6:M7";
-const GOOGLE_SHEET_QC_PERFORMANCE_TABLE_RANGE = "Team Estimates!A18:M19";
+const GOOGLE_SHEET_TECH_PERFORMANCE_TABLE_RANGE = "Team Estimates!A6:N7";
+const GOOGLE_SHEET_QC_PERFORMANCE_TABLE_RANGE = "Team Estimates!A18:N19";
 
 const IQ_API_SESSION_URL = "https://signin.iqreseller.com/api/IntegrationAPI/Session";
 const IQ_GET_INVENTORIES_BY_PO_ULR = 'https://api.iqreseller.com/webapi.svc/Inventory/JSON/GetInventoriesByPOId';
